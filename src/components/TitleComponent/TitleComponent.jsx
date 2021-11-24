4cc65832d1b6050247836c87a098249f709b0f70
PrashantSedhain <prashantased@gmail.com>
2021-11-23 22:04:49 -0600

    Changed top padding for title component

--- src/components/TitleComponent/TitleComponent.jsx
+++ src/components/TitleComponent/TitleComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TitleComponent = (props) => {
     return (
         <div>
-            <h2 style={{fontSize: "30px", color: "maroon", textAlign: "center", fontFamily: "cursive",padding: "0.7em"}}>{props.title}</h2>
+            <h2 style={{fontSize: "30px", color: "maroon", textAlign: "center", fontFamily: "cursive", paddingTop: "1em"}}>{props.title}</h2>
             <hr />
         </div>
     )
