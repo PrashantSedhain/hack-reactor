e5d6c8be22e7163d6a94c7dd63f11dcbcad871ac
PrashantSedhain <prashantased@gmail.com>
2021-11-23 22:12:54 -0600

    Dynamically rendered Date Input Fields

--- src/components/SearchBarComponent/SearchBarComponent.jsx
+++ src/components/SearchBarComponent/SearchBarComponent.jsx
@@ -3,28 +3,43 @@ import { Button } from "react-bootstrap";
 import DateInputComponent from "../DateInputComponent/DateInputComponent";
 
 const SearchBarComponent = (props) => {
+    
+  const dateFields = [
+    {
+      id: "start-date",
+      label: "Select Start Date",
+      setDate: props.setStartDate,
+      selectedDate: props.selectedStartDate,
+    },
+    {
+      id: "end-date",
+      label: "Select End Date",
+      setDate: props.setEndDate,
+      selectedDate: props.selectedEndDate,
+    },
+  ];
+
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <DateInputComponent
-          id="start-date"
-          label="Select Start Date"
-          setDate={props.setStartDate}
-          selectedDate={props.selectedStartDate}
-        />
-        <DateInputComponent
-          id="end-date"
-          label="Select End Date"
-          setDate={props.setEndDate}
-          selectedDate={props.selectedEndDate}
-        />
+        {dateFields.map((item) => {
+          return (
+            <DateInputComponent
+              key={item.id}
+              id={item.id}
+              label={item.label}
+              setDate={item.setDate}
+              selectedDate={item.selectedDate}
+            />
+          );
+        })}
+        ;
       </div>
-      <div style={{textAlign: "center"}}> 
-      <Button onClick={props.onSearchClicked} variant="outline-dark">
-        Search
-      </Button>
+      <div style={{ textAlign: "center" }}>
+        <Button onClick={props.onSearchClicked} variant="outline-dark">
+          Search
+        </Button>
       </div>
-
     </div>
   );
 };
